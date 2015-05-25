af17cedfcee7f23dfeefc258ec5ea745b491b5ce
blisek <bartlomiej.lisek@hotmail.com>
2015-05-25 19:24:45 +0200

    feats base class

---
+++ src/main/java/com/blisek/dnd3manager/dnd3/AbstractFeat.java
@@ -0,0 +1,34 @@
+package com.blisek.dnd3manager.dnd3;
+
+public abstract class AbstractFeat implements SystemObject {
+
+	@Override
+	public boolean isOnFor(CreatureModel model, Object... extraParams) {
+		if(model == null)
+			throw new NullPointerException();
+		return true;
+	}
+	
+	/**
+	 * Informuje czy atut jest pasywny (działa non-stop).
+	 * @return true jeśli jest pasywny inaczej false.
+	 */
+	public abstract boolean isPassive();
+	
+	/**
+	 * Jeśli atut jest aktywny (niepasywny) ta metoda aktywuje wszystkie modyfikatory
+	 * dane przezeń. Jeśli jest pasywny nie powinien nic robić.
+	 * @param model model dla którego atut zostanie aktywowany.
+	 * @param extraParams dodatkowe parametry.
+	 */
+	public void activateFor(CreatureModel model, Object... extraParams) {}
+	
+	/**
+	 * Deaktywuje wcześniej aktywowany atut dla danego modelu. Jeśli atut
+	 * nie był wcześniej aktywowany metoda nie powinna w żaden sposób wpłynąć
+	 * na zmianę wewnętrznego stanu modelu.
+	 * @param model model postaci.
+	 * @param extraParams dodatkowe parametry.
+	 */
+	public void deactivateFor(CreatureModel model, Object... extraParams) {}
+}
---
+++ src/main/java/com/blisek/dnd3manager/dnd3/SystemObject.java
@@ -0,0 +1,34 @@
+package com.blisek.dnd3manager.dnd3;
+
+public interface SystemObject {
+	
+	/**
+	 * Zwraca nazwę systemową.
+	 * @return łańcuch znaków z nazwą systemową.
+	 */
+	public String getSystemName();
+	
+	/**
+	 * Sprawdza czy efekt/obiekt jest włączony dla danej postaci.
+	 * @param model model postaci.
+	 * @param extraParams dodatkowe parametry dla metody.
+	 * @return true jeśli efekt/obiekt jest przypisany do postaci inaczej false.
+	 */
+	public boolean isOnFor(CreatureModel model, Object... extraParams);
+	
+	/**
+	 * Przypisuje postaci ten efekt.
+	 * @param model model postaci.
+	 * @param extraParams dodatkowe parametry.
+	 */
+	public void turnOnFor(CreatureModel model, Object... extraParams);
+	
+	/**
+	 * Wyłącza efekt dla danej postaci. Jeśli efekt nie był wcześniej
+	 * włączony możliwe jest, że ta metoda zmieni stan wewnętrzny modelu,
+	 * nawet uszkadzając go.
+	 * @param model model postaci.
+	 * @param extraParams dodatkowe parametry.
+	 */
+	public void turnOffFor(CreatureModel model, Object... extraParams);
+}
