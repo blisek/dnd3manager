8865f60941d51de053d7ec03db69669b54705054
blisek <bartlomiej.lisek@hotmail.com>
2015-05-26 23:29:59 +0200

    klasa Context

---
+++ src/main/java/com/blisek/dnd3manager/dnd3/Context.java
@@ -0,0 +1,13 @@
+package com.blisek.dnd3manager.dnd3;
+
+/**
+ * Klasa zawierająca kontekst, używany przez inne
+ * narzędzia, udostępniający informacje o wszystkich,
+ * w ramach tego kontekstu, dostępnych klasach, rasach,
+ * atutach itp.
+ * @author bartek
+ *
+ */
+public class Context {
+
+}
