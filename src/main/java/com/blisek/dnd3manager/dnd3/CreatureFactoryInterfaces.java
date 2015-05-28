c280ed69591ab26148236cb032ae9f4dcf2bf44e
blisek <bartlomiej.lisek@hotmail.com>
2015-05-28 23:13:18 +0200

    metody wytwórcze postaci

---
+++ src/main/java/com/blisek/dnd3manager/dnd3/CreatureFactoryInterfaces.java
@@ -0,0 +1,70 @@
+package com.blisek.dnd3manager.dnd3;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class CreatureFactoryInterfaces {
+	
+	/**
+	 * Metoda wytwórcza zwracająca wartości
+	 * do użycia w atrybutach.
+	 * @author bartek
+	 *
+	 */
+	public static interface AbilitiesFactory {
+		public int[] getAbilities(Map<String, Object> extraParams);
+	}
+	
+	/**
+	 * Metoda wytwórcza zwracająca rasę.
+	 * @author bartek
+	 *
+	 */
+	public static interface RaceFactory {
+		public AbstractRace getRace(CreatureModel model, Map<String, Object> extraParams);
+	}
+	
+	/**
+	 * Metoda wytwórcza dająca klasę postaci.
+	 * @author bartek
+	 *
+	 */
+	public static interface ClassFactory {
+		public AbstractClass getClass(CreatureModel model, Map<String, Object> extraParams);
+	}
+	
+	/**
+	 * Metoda przyporządkowująca, wcześniej wylosowane, wartości
+	 * do konkretnych atrybutów.
+	 * @author bartek
+	 *
+	 */
+	public static interface AbilitiesSelectionFactory {
+		public void selectAbilities(CreatureModel model, int[] abilities, Map<String, Object> extraParams);
+	}
+	
+	/**
+	 * Wybiera umiejętności i przypisuje je modelowi.
+	 * @author bartek
+	 *
+	 */
+	public static interface SkillsFactory {
+		public void selectSkills(CreatureModel model, int ranks, Map<String, Object> extraParams);
+	}
+	
+	/**
+	 * Wybiera atuty.
+	 * @author bartek
+	 *
+	 */
+	public static interface FeatsFactory {
+		/**
+		 * Wybiera atuty i przypisuje je do modelu. Wybór można ograniczyć
+		 * do iterowalnej kolekcji przekazanej w argumencie limitation.
+		 * @param model model postaci.
+		 * @param num_of_feats liczba atutów do wyboru.
+		 * @param limitation kolekcja z której będą wybierane atuty.
+		 */
+		public void selectFeats(CreatureModel model, int num_of_feats, Optional<Iterable<AbstractFeat>> limitation);
+	}
+}
