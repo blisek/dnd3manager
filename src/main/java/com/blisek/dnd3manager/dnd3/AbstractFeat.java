552e3101b4f839ba5aca8193444081696e13e4a7
blisek <bartlomiej.lisek@hotmail.com>
2015-06-02 22:24:00 +0200

    poszerzenie interfejsu AbstractFeat o dodatkowe metody

--- src/main/java/com/blisek/dnd3manager/dnd3/AbstractFeat.java
+++ src/main/java/com/blisek/dnd3manager/dnd3/AbstractFeat.java
@@ -18,6 +18,19 @@ public abstract class AbstractFeat implements RestrictedSystemObject {
 	public abstract boolean isPassive();
 	
 	/**
+	 * Informuje czy model w swoim aktualnym stanie może mieć aktywowany
+	 * ten atut. Domyślnie zwraca wartość przeciwną do isPassive, czyli
+	 * domyślnie jeśli atut może być aktywowany, to metoda zwraca true
+	 * za każdym razem.
+	 * @param model model postaci.
+	 * @param extraParams dodatkowe parametry.
+	 * @return true jeśli można aktywować go dla obecnego stanu modelu postaci.
+	 */
+	public boolean canBeActivatedFor(CreatureModel model, Map<String, Object> extraParams) {
+		return !isPassive();
+	}
+	
+	/**
 	 * Jeśli atut jest aktywny (niepasywny) ta metoda aktywuje wszystkie modyfikatory
 	 * dane przezeń. Jeśli jest pasywny nie powinien nic robić.
 	 * @param model model dla którego atut zostanie aktywowany.
@@ -33,4 +46,14 @@ public abstract class AbstractFeat implements RestrictedSystemObject {
 	 * @param extraParams dodatkowe parametry.
 	 */
 	public void deactivateFor(CreatureModel model, Map<String, Object> extraParams) {}
+	
+	/**
+	 * Zwraca typ atutu. Domyślnie FeatType.GENERAL. 
+	 * Na podstawie zwracanego typu można określić
+	 * podklasę, jeśli występuje.
+	 * @return typ atutu.
+	 */
+	public FeatType getFeatType() {
+		return FeatType.GENERAL;
+	}
 }
